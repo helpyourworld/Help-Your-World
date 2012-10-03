bc991555cb1d5a2befbd4fc6e9d8fb38b9ea987d
localpopularart.com <localpopularart.com@n10.c05.mtsvc.net>
2012-10-03 14:30:01 -0700

    Removing Joomla 1.5 files

--- templates/rt_tachyon_j15/html/com_discussions/board/edit.php
+++
@@ -1,9 +0,0 @@
-<?php defined('KOOWA') or die('Restricted access');?>
-
-<?= $application_header ?>
-<?= $actor_header ?>
-
-<div id="an-discus-board-edit" class="an-se-pane an-se-rounded rounded">
-	<h2><?= @text('AN-DISCUSSIONS-BOARD-EDIT') ?></h2>
-	<?= @template('form') ?>
-</div>
--- templates/rt_tachyon_j15/html/com_socialengine/people/search.php
+++
@@ -1,6 +0,0 @@
-<?php defined('KOOWA') or die; ?>
-
-<?= $header ?>
-<?= @get('lib.anahita.sekit.search.view.form')->setSearchPath('index.php?option=com_socialengine&view=people&layout=list') ?>
-
-<div id="an-se-entities-wrapper" class="an-se-entities-wrapper"></div>
